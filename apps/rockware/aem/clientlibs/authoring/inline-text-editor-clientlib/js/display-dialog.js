f66377d1b4ada177b788876910cee8c86c2b744e
spakira <36262011+spakira@users.noreply.github.com>
2019-02-08 09:28:18 +0100

    Update display-dialog.js

--- apps/rockware/aem/clientlibs/authoring/inline-text-editor-clientlib/js/display-dialog.js
+++ apps/rockware/aem/clientlibs/authoring/inline-text-editor-clientlib/js/display-dialog.js
@@ -1,5 +1,5 @@
 (function($,$document){
-    var SENDER = "sumanta-showcase",
+    var SENDER = "rockware-showcase",
         REQUESTER = "requester",
         COLOR = "color",
         FONT_SIZE = "font-size",
